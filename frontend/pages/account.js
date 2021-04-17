854b53cb67f36701052225745e0868c4b6882c97
cokoyoh <charlesokoyoh@gmail.com>
2021-04-17 11:46:40 +0300

    Add account, orders and products pages to next.js

---
+++ frontend/pages/account.js
@@ -0,0 +1,7 @@
+const Account = () => (
+  <div>
+    <p>Accounts!!</p>
+  </div>
+)
+
+export default Account;
---
+++ frontend/pages/orders.js
@@ -0,0 +1,9 @@
+const Orders = () => {
+  return (
+    <div>
+      <p>Orders Page!</p>
+    </div>
+  )
+}
+
+export default Orders;
